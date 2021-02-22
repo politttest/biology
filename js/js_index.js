1990c31783aa7ab1ad410d10f7097b56d091aea8
politttest <77047995+politttest@users.noreply.github.com>
2021-02-22 23:00:59 +0200

    Add files via upload

---
+++ js/js_index.js
@@ -0,0 +1,7 @@
+function enter(){
+    var input = document.getElementById("one_input").value
+    var final = document.getElementById("final")
+    if(input == "УУУ"){
+        final.innerHTML += "Фен"
+    }
+}
